296ddd34b9ce1f894f9d9ed824abb978a8c68266
pk123 <mailid>
2023-02-04 20:15:57 +0530

    modified

---
+++ src/test/java/com/actitime/testscript/ModifycustomerTest.java
@@ -0,0 +1,13 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ModifycustomerTest {
+	@Test
+	public void modifyCus()
+	{
+		//modify
+		Reporter.log("cus",true);	
+	}
+}
